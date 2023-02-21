bec4ff72636f6e376b8de0313290880eb92e535b
huska20 <sharifka87@gmail.com>
2023-02-21 11:38:28 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_aphabet - make the alphabet
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = '0'; c <= 'z'; c++)
+	{
+	putchar(c);
+
+	_putchar('\n');
+	}
+}
