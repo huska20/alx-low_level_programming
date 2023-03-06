b18dcc880409ba55ebd60e03268a099901bf4068
huska20 <sharifka87@gmail.com>
2023-03-06 08:19:54 -0800

    0-memset

--- 0x07-pointers_arrays_strings/0-main.c
+++ 0x07-pointers_arrays_strings/0-main.c
@@ -21,12 +21,12 @@ void simple_print_buffer(char *buffer, unsigned int size)
 		}
 		if (!(i % 10) && i)
 		{
-			 printf("\n");
+			printf("\n");
 		}
-		 printf("0x%02x", buffer[i]);
-		 i++;
+		printf("0x%02x", buffer[i]);
+		i++;
 	}
-	 printf("\n");
+	printf("\n");
 }
 
 /**
